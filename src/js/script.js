e1404c713fd5c5d0e32bd2995c8e147641aafeae
NinaNaumova <naumova.ninna@gmail.com>
2022-05-31 23:53:15 +0300

    Add tabs

--- src/js/script.js
+++ src/js/script.js
@@ -14,4 +14,10 @@ $(document).ready(function(){
             }
         ]
       });
+
+      $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+      });
   });
