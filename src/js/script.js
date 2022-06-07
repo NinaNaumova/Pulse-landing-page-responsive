11a77d54fc00d380f5d8c515914acbf2c28fe18a
NinaNaumova <naumova.ninna@gmail.com>
2022-06-07 19:54:08 +0300

    Add smart.php

--- src/js/script.js
+++ src/js/script.js
@@ -76,4 +76,24 @@ $(document).ready(function(){
 	ValideForms('#order form');
 
 	$('input[name=phone]').mask("+7 (999) 999-99-99");
+
+	$('form').submit(function(e) {
+		e.preventDefault();
+
+		if (!$(this).valid()) {
+			return;
+		}
+
+		$.ajax({
+			type:"POST",
+			url: "mailer/smart.php",
+			data: $(this).serialize()
+		}).done(function() {
+			$(this).find("input").val("");
+			$('#consultation, #order').fadeOut();
+			$('.overlay, #thanks').fadeIn('slow');
+			$('form').trigger('reset');
+		});
+		return false;
+	});
 });
