88791198e292a8680ae98f2de55d3dad581d45f0
NinaNaumova <naumova.ninna@gmail.com>
2022-06-08 16:44:36 +0300

    Add pageup

--- src/js/script.js
+++ src/js/script.js
@@ -96,4 +96,20 @@ $(document).ready(function(){
 		});
 		return false;
 	});
+
+	//Smooth scroll and pageup
+
+	$(window).scroll(function() {
+		if ($(this).scrollTop() >1600) {
+			$('.pageup').fadeIn();
+		} else {
+			$('.pageup').fadeOut();
+		}
+	});
+
+	$("a[href=#up]").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
 });
